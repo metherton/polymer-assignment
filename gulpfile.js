f9c877da290f0ac86696f32f861398cbdb3ef958
metherton <martinetherton63@gmail.com>
2017-09-12 22:15:14 +0200

    added filters

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,24 @@
 'use strict';
 var gulp        = require('gulp');
+var filters     = require('gulp-filter');
 var insert      = require('gulp-insert');
 var sass        = require('gulp-sass');
 var $           = require('gulp-load-plugins')();
 var runSequence = require('run-sequence');
 
 
+var filter = filters(['*', '!app/css/skins/color-vars-template.scss', '!app/css/skins/skin-template.scss']);
+
+
 gulp.task('sass', function () {
     gulp.src('./app/css/*.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(filter)
         .pipe(gulp.dest('app/css'));
 
     gulp.src('app/css/skins/default/*.scss')
-        .pipe(sass())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(filter)
         .pipe(gulp.dest('app/css/skins/default'));
 });
 
